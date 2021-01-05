4a4116783cf6e39b0c59127b476824f720eafae9
hobian <okhobian@gmail.com>
2021-01-05 12:35:47 -0600

    question 347

---
+++ top100/347_top_k_freq_elemets/topK.java
@@ -0,0 +1,49 @@
+class Solution 
+{
+    /**
+     * bucket sort approach
+     * time = O(n)
+     * space = O(n)
+     */
+
+    public int[] topKFrequent(int[] nums, int k) 
+    {
+        // map --> element:frequency
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums)
+            map.put(num, map.getOrDefault(num, 0)+1);
+        
+        // bucket --> index=frequency; listAtIndex=elementsWithThisFreq
+        ArrayList<Integer>[] list = new ArrayList[nums.length+1];
+        for (Map.Entry<Integer, Integer> entry : map.entrySet())
+        {
+            int num = entry.getKey();
+            int freq = entry.getValue();
+            
+            if (list[freq] == null)
+                list[freq] = new ArrayList();
+            
+            list[freq].add(num);
+        }
+        
+        // result --> iterate from last (most freq), get up to k frequent elements
+        // i: bucket array counter
+        // j: result array counter
+        // k: result array size
+        // l: list at bucket[i] counter
+        int[] res = new int[k];
+        for (int i=list.length-1, j=0; i>0; i--)
+        {
+            if (j>=k) break;
+            int l = 0;
+            while (list[i]!=null && j<k && l<list[i].size())
+            {
+                res[j] = list[i].get(l);
+                j++;
+                l++;
+            }
+        }
+    
+        return res;
+    }
+}
