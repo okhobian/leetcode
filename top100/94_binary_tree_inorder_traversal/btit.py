27b2f7805ba64bc79433ef4876b5bf5303b1de7d
okhobian <76004407+okhobian@users.noreply.github.com>
2022-09-08 11:22:40 -0500

    Create btit.py

---
+++ top100/94_binary_tree_inorder_traversal/btit.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if not root: return []
+        
+        res = []
+        self.helper(res, root)
+        return res
+        
+    def helper(self, res, node):
+        if not node: return
+        
+        self.helper(res, node.left)
+        res.append(node.val)
+        self.helper(res, node.right)
+        
